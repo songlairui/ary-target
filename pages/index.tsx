3683c0fce2f4823d072d960f42811a274b775784
songlairui <cnre@live.cn>
2019-09-03 16:02:25 +0800

    use useRouter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,29 @@
 import { NextPage } from 'next'
-
-import Header from '../components/Header'
+import { FunctionComponent } from 'react'
+import Link from 'next/link'
 import Layout from '../components/Layout'
+
+type Props = {
+    title?: string
+}
+const PostLink: FunctionComponent<Props> = (props) => (
+    <li>
+        <Link href={`/post?title=${props.title}`}>
+            <a>{props.title}</a>
+        </Link>
+    </li>
+)
 
 const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
     <Layout>
         <h1>Hello world!</h1>
         <small>user agent {userAgent}</small>
+        <hr />
+        <ul>
+            <PostLink title="hello01"></PostLink>
+            <PostLink title="hello02"></PostLink>
+            <PostLink title="hello03"></PostLink>
+        </ul>
     </Layout>
 )
 
---
+++ pages/post.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import Layout from '../components/Layout'
+
+const Page: NextPage = () => {
+    const router = useRouter()
+    return (
+        <Layout>
+            <h1>{router.query.title}</h1>
+            <p>This is the blog post content</p>
+        </Layout>
+    )
+}
+export default Page
