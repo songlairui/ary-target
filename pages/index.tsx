8d2692540015d6c9fd970442ca61682d6ef3cf21
songlairui <songlairui@126.com>
2019-09-05 18:25:02 +0800

    add index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,69 +1,34 @@
-import {
-  Form,
-  Select,
-  InputNumber,
-  DatePicker,
-  Switch,
-  Slider,
-  Button
-} from "antd";
+import Link from "next/link";
+import { Layout, Card, Button } from "antd";
 
-const FormItem = Form.Item;
-const Option = Select.Option;
+const { Header, Sider, Content, Footer } = Layout;
 
 export default () => (
-  <div style={{ marginTop: 100 }}>
-    <Form layout="horizontal">
-      <FormItem
-        label="Input Number"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 8 }}
-      >
-        <InputNumber
-          size="large"
-          min={1}
-          max={10}
-          style={{ width: 100 }}
-          defaultValue={3}
-          name="inputNumber"
-        />
-        <a href="#">Link</a>
-      </FormItem>
-
-      <FormItem label="Switch" labelCol={{ span: 8 }} wrapperCol={{ span: 8 }}>
-        <Switch defaultChecked />
-      </FormItem>
-
-      <FormItem label="Slider" labelCol={{ span: 8 }} wrapperCol={{ span: 8 }}>
-        <Slider defaultValue={70} />
-      </FormItem>
-
-      <FormItem label="Select" labelCol={{ span: 8 }} wrapperCol={{ span: 8 }}>
-        <Select size="large" defaultValue="lucy" style={{ width: 192 }}>
-          <Option value="jack">jack</Option>
-          <Option value="lucy">lucy</Option>
-          <Option value="disabled" disabled>
-            disabled
-          </Option>
-          <Option value="yiminghe">yiminghe</Option>
-        </Select>
-      </FormItem>
-
-      <FormItem
-        label="DatePicker"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 8 }}
-      >
-        <DatePicker name="startDate" />
-      </FormItem>
-      <FormItem style={{ marginTop: 48 }} wrapperCol={{ span: 8, offset: 8 }}>
-        <Button size="large" type="primary" htmlType="submit">
-          OK
-        </Button>
-        <Button size="large" style={{ marginLeft: 8 }}>
-          Cancel
-        </Button>
-      </FormItem>
-    </Form>
-  </div>
+  <Layout style={{ height: "100%" }}>
+    <Header style={{ height: "20vh", background: "#fff", padding: 0 }}></Header>
+    <Layout>
+      <Sider theme="light"></Sider>
+      <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+        <Card style={{ width: 300 }} bordered={false}>
+          <Link href="/self-speak">
+            <Button type="link" block>
+              Self Speak
+            </Button>
+          </Link>
+        </Card>
+      </Content>
+      <Sider theme="light"></Sider>
+    </Layout>
+    <Footer style={{ textAlign: "center", backgroundColor: "#fff" }}>
+      {/* ©2019 Songlairui */}
+      {""}
+    </Footer>
+    <style jsx global>
+      {`
+        #__next {
+          height: 100%;
+        }
+      `}
+    </style>
+  </Layout>
 );
---
+++ pages/self-speak.tsx
@@ -0,0 +1,1 @@
+export default () => <div>Speak to mySelf</div>;
