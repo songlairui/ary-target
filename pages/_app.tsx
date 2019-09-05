b961ce352c173cf114fc7a410493150fca2f86cd
songlairui <songlairui@126.com>
2019-09-05 18:42:27 +0800

    add Page

---
+++ pages/_app.tsx
@@ -0,0 +1,4 @@
+import App from "next/app";
+import "../assets/global.less";
+
+export default App;
--- pages/self-speak.tsx
+++ pages/self-speak.tsx
@@ -1,1 +1,33 @@
-export default () => <div>Speak to mySelf</div>;
+import { Layout, List, Row, Col } from "antd";
+
+const { Header, Sider, Content } = Layout;
+const data = [
+  "其实，我想要我做的事情有进度感",
+  "Actually, I have a sense of advancement"
+];
+
+export default () => (
+  <Layout style={{ height: "100%" }}>
+    <Header style={{ height: "20vh", background: "#fff", padding: 0 }}></Header>
+    <Layout>
+      <Sider theme="light"></Sider>
+      <Content
+        style={{
+          overflow: "initial",
+          backgroundColor: "#fff"
+        }}
+      >
+        <Row type="flex" justify="space-around">
+          <Col style={{ maxWidth: "43em", minWidth: "13em" }}>
+            <List
+              bordered
+              dataSource={data}
+              renderItem={item => <List.Item>{item}</List.Item>}
+            />
+          </Col>
+        </Row>
+      </Content>
+      <Sider theme="light"></Sider>
+    </Layout>
+  </Layout>
+);
